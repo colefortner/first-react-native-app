10538b523de158841c0af81762bd08636d1daaa6
Cole Fortner <colefortner@gmail.com>
2021-10-21 23:52:56 -0400

    Added some janky navigation, want to save this example for later reference

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import React from 'react';
-import {Button, StyleSheet, Text, SafeAreaView, ScrollView} from 'react-native';
+import React, {useState} from 'react';
+import {Button, StyleSheet, Text, SafeAreaView} from 'react-native';
 
 const App = () => {
+  // he says do not do navigation this way
+  const [activeScreen, setActiveScreen] = useState('home');
+
   return (
-    <SafeAreaView>
-      <ScrollView style={styles.container}>
-        <Text style={styles.helloText}>Hello</Text>
-        <Text style={styles.helloText}>React</Text>
-        <Text style={styles.helloText}>Jax</Text>
-        <Text style={styles.helloText}>Jax</Text>
-        <Text style={styles.helloText}>Jax</Text>
-        <Text style={styles.helloText}>Jax</Text>
-        <Text style={styles.helloText}>Jax</Text>
-        <Text style={styles.helloText}>Jax</Text>
-        <Button title="Press Me If You Can" />
-      </ScrollView>
+    // home
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.helloText}>🐶 Bark!</Text>
+      {/* <Button title="Home" onPress={() => setActiveScreen('home')} /> */}
+      <Button title="Add a Dog" onPress={() => setActiveScreen('add')} />
+      <Button title="Show my Dogs" onPress={() => setActiveScreen('mybeers')} />
+      {activeScreen === 'add' && <AddDog />}
+      {activeScreen === 'mybeers' && <MyDogs />}
+      {/* {activeScreen === 'home' && <App />} */}
+    </SafeAreaView>
+  );
+};
+
+const AddDog = () => {
+  // add
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.helloText}>Add a 🐶</Text>
+    </SafeAreaView>
+  );
+};
+
+const MyDogs = () => {
+  // mybeers
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.helloText}>My 🐶s</Text>
     </SafeAreaView>
   );
 };
@@ -23,7 +41,11 @@ export default App;
 
 const styles = StyleSheet.create({
   container: {
+    // this makes it take up the entire screen
+    flex: 1,
     backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   helloText: {
     color: 'red',
